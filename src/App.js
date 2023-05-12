21972be40dd76ae35be77d731a76d654c66b95c5
naehyun25 <yoon658@naver.com>
2023-05-12 10:52:34 +0900

    React Tailwind Project 초기환경설정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">안녕하세요</h1>
+    </div>
+  );
+}
+
+export default App;
